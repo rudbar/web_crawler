0242ce637e2942538e31416f118c69303769c208
Ian Kachan <kachan.yanf@gmail.com>
2020-06-02 23:42:09 +0500

    Styled index page

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pisyun</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+</head>
+<body>
+
+	<div class="wrapper indexPage">
+
+		<div class="mainSection">
+			
+
+			<div class="logoContainer">
+				<img src="https://i.pinimg.com/originals/a9/9b/50/a99b50f3834b602dc10e5321b12d5e58.png">
+			</div>
+
+			<div class="searchContainer">
+
+				<form action="search.php" method="GET">
+
+					<input class="searchBox" type="text" name="term">
+					<input class="searchButton" type="submit" value="Найти">
+
+
+				</form>
+				
+				
+			</div>
+
+
+		</div>
+
+	</div>
+
+</body>
+</html>
