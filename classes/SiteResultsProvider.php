7a82c41554fd24903fef157a141a8c8358af848e
Ian Kachan <kachan.yanf@gmail.com>
2020-06-08 02:22:15 +0500

    Outputting the search results

--- classes/SiteResultsProvider.php
+++ classes/SiteResultsProvider.php
@@ -23,6 +23,49 @@ class SiteResultsProvider {
 
 	}
 
+	public function getResultsHtml($page, $pageSize, $term) {
+
+		$query = $this->con->prepare("SELECT * 
+										 FROM sites WHERE title LIKE :term
+										 OR url LIKE :term
+										 OR keywords LIKE :term
+										 OR description LIKE :term
+										 ORDER BY clicks DESC");
+		$searchTerm = "%" . $term . "%";
+		$query->bindParam(":term", $searchTerm);
+		$query->execute();
+
+
+		$resultsHtml = "<div class='siteResults'>";
+
+
+		while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+			$id = $row["id"];
+			$url = $row["url"];
+			$title = $row["title"];
+			$description = $row["description"];
+
+			$resultsHtml .= "<div class='resultContainer'> 
+
+								<h3 class='title'>
+									<a class='result' href='$url'>
+										$title
+									</a>
+								</h3>
+								<span class='url'>$url</span>
+								<span class='description'>$description</span>
+
+							</div>";
+
+		}
+
+
+
+		$resultsHtml .= "</div>";
+
+		return $resultsHtml;
+	}
+
 
 
 
--- search.php
+++ search.php
@@ -96,7 +96,14 @@ include("classes/SiteResultsProvider.php");
 			<?php
 			$resultsProvider = new SiteResultsProvider($con);
 
-			echo $resultsProvider->getNumResults($term);
+			$numResults = $resultsProvider->getNumResults($term);
+
+			echo "<p class='resultsCount'>$numResults results found</p>";
+
+
+
+
+			echo $resultsProvider->getResultsHtml(1, 20, $term);
 			?>
 
 
